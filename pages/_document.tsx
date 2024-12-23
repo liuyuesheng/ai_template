f373630e2db911fc9324ac128afa26c5c527e356
liu <6@qq.com>
2024-12-23 23:21:02 +0800

    09

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,18 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1529559601110496"
           crossOrigin="anonymous"
         />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3M5EPLJ3K5" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3M5EPLJ3K5');
+            `
+          }}
+        />
       </Head>
       <body>
         <Main />
